13932b46b1724742f325d37bb81428800a038852
Rubymoyes <moyesruby@gmail.com>
2018-10-26 11:06:54 +1300

    show movies route

--- routes/main.js
+++ routes/main.js
@@ -1,28 +1,34 @@
 const express = require('express')
 const router = express.Router()
 const fs = require('fs')
+const dataStorage = require('../data-storage')
 
-router.get('/', function(req, res){
-  let viewData = {names: [
-    {
-      name: 'Ruby'
-    },
-    {
-      name: 'Joan'
-    },
-    {
-      name: 'James'
-    },
-    {
-      name: 'Peter'
-    }
-  ]
+router.get('/', function (req, res) {
+  let viewData = {
+    names: [
+      {
+        name: 'Ruby'
+      },
+      {
+        name: 'Joan'
+      },
+      {
+        name: 'James'
+      },
+      {
+        name: 'Peter'
+      }
+    ]
   }
   res.render('index', viewData)
 })
 
-
+router.get('/movies', function (req, res) {
+  dataStorage.getMovies(function (movies) {
+    res.send('the movies' + JSON.stringify(movies))
+  })
+})
 
 module.exports = {
-    router: router
+  router: router
 }
