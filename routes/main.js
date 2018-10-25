1ca90d95274ae5859c60c801acf09b5ca9083348
Rubymoyes <moyesruby@gmail.com>
2018-10-26 11:43:44 +1300

    added movie page, added form add movie

--- routes/main.js
+++ routes/main.js
@@ -25,9 +25,24 @@ router.get('/', function (req, res) {
 
 router.get('/movies', function (req, res) {
   dataStorage.getMovies(function (movies) {
-    res.send('the movies' + JSON.stringify(movies))
+    // res.send('the movies' + JSON.stringify(movies))\
+    res.render('movies/index', { movies: movies })
   })
 })
+
+
+router.get('/movies/edit', function (req, res) {
+  res.render('movies/edit')
+})
+
+router.get('/movies/:id', function (req, res) {
+  dataStorage.getSingleMovie(req.params.id, function (movie) {
+    res.render('movies/view', movie)
+  })
+})
+
+
+
 
 module.exports = {
   router: router
